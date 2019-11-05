430872c76a372a6ad8343212435a9258c42987cc
liberalman <zscchina@163.com>
2019-11-05 16:50:08 +0800

    search

--- front/src/views/Search.vue
+++ front/src/views/Search.vue
@@ -9,7 +9,7 @@
         <h1 class="shaddow">{{headline}}</h1>
         <p class="shadow-light">{{subline}}</p>
         <br/>
-        <iframe src="https://www.hicool.top/libertyblog-search/search.php" width="1280" height="580" frameborder=0 scrolling=auto></iframe>
+        <iframe src="https://www.hicool.top/libertyblog-search/search.php" width="1180" height="580" frameborder=0 scrolling=auto></iframe>
       </div>
     </header>
   </div>
@@ -25,8 +25,6 @@ export default {
     return {
       headline: 'Hicool Search', // 用户名
       subline: 'The thought of independence and freedom.', // 副标题
-      imgSrc: 'http://image.hicool.top/static/album/5ac0a4bac0979028323030f3/1503759063328965200.jpg', // 头图 http://of30nsqpd.bkt.clouddn.com/2015061101335924.jpeg
-      
       value: '',
       suggestionAttribute: 'original_title',
       suggestions: [],
@@ -34,12 +32,18 @@ export default {
     }
   },
   computed: Vuex.mapState({
-    
+    imgSrc: state => state.tip.cover,
   }),
   created() {
-    
+
+  },
+  mounted() {
+    this.fetchData()
   },
   methods: {
+    fetchData() {
+      this.$store.dispatch('tip/getTipIndex')
+    },
     clickInput () {
       this.selectedEvent = 'click input'
     },
