030c5598bd17af56e4e2361bdaece8fb99621245
liberalman <zscchina@163.com>
2021-05-07 20:31:54 +0800

    ok

--- front/config/webpack.base.conf.js
+++ front/config/webpack.base.conf.js
@@ -48,17 +48,16 @@ module.exports = {
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
-        query: {
-          limit: 10000,
-          //name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        options: {
+          limit: 10*1024,
           name: 'static/images/[name].[hash:7].[ext]'
         }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
-        query: {
-          limit: 10000,
+        options: {
+          limit: 10*1024,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       },
